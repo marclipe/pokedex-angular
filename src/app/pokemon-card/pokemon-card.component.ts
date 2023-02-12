bf3f0d50052ce487abf6077015105716619d9cbc
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-02-12 14:39:37 -0300

    component of card and list Pokemon

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass']
+})
+export class PokemonCardComponent {
+  @Input()
+  pokemon: string; 
+
+  constructor() { 
+    
+  }
+}
